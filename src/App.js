de69e8bc5a2cb704d81f9a43ec9018aff441a430
Readul Huq Khan <readz07@gmail.com>
2022-07-03 16:30:47 +0600

    daisy ui and pages setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Header from './Pages/Common/Header/Header';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home/Home';
+import About from './Pages/Home/About/About';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,31 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#f97316",
+
+          "secondary": "#fb923c",
+
+          "accent": "#EEAF3A",
+
+          "neutral": "#ea580c",
+
+          "base-100": "#f3f4f6",
+
+          "info": "#3ABFF8",
+
+          "success": "#65a30d",
+
+          "warning": "#FBBD23",
+
+          "error": "#be123c",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 }
