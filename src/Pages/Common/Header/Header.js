756f58d1182240057785881d765e8349af420468
Readul Huq Khan <readz07@gmail.com>
2022-07-03 18:40:45 +0600

    new product section

--- src/Pages/Common/Header/Header.js
+++ src/Pages/Common/Header/Header.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 const Header = () => {
     const topNav = <>
-
+        <li><a>Item 1</a></li>
+        <li tabindex="0">
+            <a className="justify-between">
+                Parent
+                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+            </a>
+            <ul className="p-2">
+                <li><a>Submenu 1</a></li>
+                <li><a>Submenu 2</a></li>
+            </ul>
+        </li>
+        <li><a>Item 3</a></li>
     </>
     return (
         <div className="navbar border-b border-rose-100 ">
@@ -12,26 +23,14 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li tabindex="0">
-                            <a className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {topNav}
                     </ul>
                 </div>
                 <p className="btn btn-ghost normal-case text-xl">B2B</p>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
-                    <li><a>Item 1</a></li>
-                    <li><a>Item 2</a></li>
+                    {topNav}
                 </ul>
             </div>
             <div className="navbar-end">
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Banners from '../Banners/Banners';
+import Stats from '../Stats/Stats';
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <Banners/>
+            <Stats/>
+        </>
     );
 };
 
